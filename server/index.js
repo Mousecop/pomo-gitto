aff8b92dba422d72085dc80c7cd8bf5426f924fb
Aaron Gottlieb <aticus19@gmail.com>
2017-03-20 21:20:54 -0600

    adds github oauth

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const passport = require('passport');
 const GitHubStrategy = require('passport-github').Strategy;
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const init = require('./init');
 
 const {User} = require('./models');
 const mongoose = require('mongoose');
@@ -11,23 +13,31 @@ const mongoose = require('mongoose');
 const app = express();
 mongoose.Promise = global.Promise;
 
+
 const {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, DATABASE_URL} = require('./config');
 
 // API endpoints go here!
 app.use(bodyParser.json())
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(new GitHubStrategy({
     clientID: 'c7b2c8e37d76f08dcb09',
     clientSecret: 'df731f1c8dbcdcd5b3f4fc4cb31678f7d2db4a4e',
     callbackURL: 'http://localhost:8080/api/auth/github/callback'
     },
-    function(accessToken, refreshToken, profile, cb) {
+    (accessToken, refreshToken, profile, cb) => {
         User.findOrCreate({githubId: profile.id}, function (err, user){
             return cb(err, user);
         });
     }
 ));
+init();
 
 app.get('/api/auth/github', passport.authenticate('github'));
 
@@ -36,6 +46,7 @@ app.get('/api/auth/github/callback',
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
+    
   });
 
 
@@ -52,7 +63,7 @@ app.get(/^(?!\/api(\/|$))/, (req, res) => {
 let server;
 function runServer(port=3001) {
     return new Promise((resolve, reject) => {
-        mongoose.connect("mongodb://user:me@ds137760.mlab.com:37760/pomo-gitto", err => {
+        mongoose.connect("mongodb://localhost/passport-social-auth", err => {
 
             if (err) {
 
--- server/models.js
+++ server/models.js
@@ -3,8 +3,8 @@ const findOrCreate = require('mongoose-findorcreate')
 
 
 const userSchema = mongoose.Schema ({
-    username: {type: String, required: true},
     githubId: Number
+
 })
 
 userSchema.plugin(findOrCreate);
