17cf3fd6321a91e98f9e7228f538c52074e19826
Mousecop <joaskew13@gmail.com>
2017-03-21 20:08:47 -0400

    Removes old state

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,10 @@ import './App.css';
 import Clock from './components/clock';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-      paused: true
-   }
-   this.handleOnClick = this.handleOnClick.bind(this);
-	}
-
-	handleOnClick() {
-    this.setState ({
-      paused: false
-    })
-    console.log('clicked ' + this.state.paused)
-}
 	render() {
-		return (
-        <Clock onClick={this.handleOnClick} paused={this.state.paused}/>
-    );
+        return (
+        <Clock />
+        );
 	}
 }
 
