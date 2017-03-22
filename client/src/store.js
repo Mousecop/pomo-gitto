b2ee3b2df0bea859b5c67acc75b3cb26844f2520
Mousecop <joaskew13@gmail.com>
2017-03-22 16:53:44 -0400

    removes whitespace

--- client/src/store.js
+++ client/src/store.js
@@ -13,9 +13,4 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers/index';
 import thunk from 'redux-thunk';
 
-
-
-
 export default createStore(reducers, applyMiddleware(thunk));
-
-
