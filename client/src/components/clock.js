c3c9737f439dbc468f81a1e4c2ab4265c47945ba
Mousecop <joaskew13@gmail.com>
2017-03-21 20:10:55 -0400

    cleans out old comments, adds MSTP and MDTP functions

--- client/src/components/clock.js
+++ client/src/components/clock.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import ReactCountdownClock from 'react-countdown-clock';
+import { toggleTime } from '../actions/action';
 
-export default function Clock (props) {
-	// let pause = true;
-	// function togglePause() {
-	// 	pause ? pause = false : pause=true;
-	// 	console.log(pause);
-	// }
+export function Clock (props) {
 	return (
         <div className="App">
             <h1>PomoGitto</h1>
-			<button type="button" onClick={props.onClick}>Start</button>
+			<button type="button" onClick={props.toggleTimeRunning}>Start</button>
 			<div className="clock">
 				<ReactCountdownClock seconds={1500}
 				size={400}
 				weight={10}
-				paused={props.paused}
-				onClick={props.onClick}/>
+				paused={!props.isTimeRunning}/>
 			</div>
 			
         </div>
     );
 }
+
+
+const mapStateToProps = (state, props) => ({
+	isTimeRunning: state.Clock.isTimeRunning
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	toggleTimeRunning() {
+		dispatch(toggleTime())
+	}
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Clock);
+
+
+
