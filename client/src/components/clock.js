710c6ca2df90d04de62058bae87346d71a694617
Mousecop <joaskew13@gmail.com>
2017-03-22 22:20:18 -0400

    makes clock class component, makes a new clock

--- client/src/components/clock.js
+++ client/src/components/clock.js
@@ -1,33 +1,76 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ReactCountdownClock from 'react-countdown-clock';
 import { toggleTime, resetClock } from '../actions/action';
 import '../imports/materialize-css/dist/css/materialize.css';
 import '../clock.css';
 
-export const Clock = (props) => {
+export class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			seconds: 5
+		}
+	}
+
+	componentDidMount() {
+		this.startCountdown();
+	}
+
+	componentWillUnmount() {
+		this.stopCountdown();
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.stopCountdown();
+		this.setState({seconds: nextProps.seconds});
+		this.startCountdown();
+ 	 }
+	
+	tick() {
+		if (this.state.seconds <= 0) { 
+			console.log('timer over!')
+			this.stopCountdown();
+		} else {
+			this.setState({
+				seconds: this.state.seconds - 1
+			});
+		}
+  	}
+  
+  startCountdown() {
+     this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  
+  stopCountdown() {
+    this.setState({seconds: 0})
+    clearInterval(this.timerID);
+  }
+
+
+	render() {
+		const minutes = Math.floor(this.state.seconds / 60);
+		const remSeconds = this.state.seconds % 60;
 		return (
-			<div className="clockContainer z-depth-3">
-				<button onClick={props.toggleTimeRunning}>Reset</button>
+			<div className=" clockContainer container z-depth-3">
 				<div className="clock">
-					<ReactCountdownClock seconds={props.time}
-					fontSize={'30px'}
-					color={'#7986cb'}
-					size={375}
-					weight={15}
-					paused={!props.isTimeRunning}
-					pausedText={'Click to Start'}
-					onComplete={props.resetClock}
-					/>
+					<span className="minutes timer flow-text">{(minutes < 10 ? '0' + minutes : minutes)}</span>
+					<span className="colon timer flow-text"> : </span>
+					<span className="seconds timer flow-text">{(remSeconds < 10 ? '0' + remSeconds : remSeconds)}</span>
 				</div>
+				<button onClick={() => {this.setState({seconds: 5})}} className="resetButton">Reset</button>
 			</div>
     	);
 	}
+		
+}
 
 
 const mapStateToProps = (state, props) => ({
 	isTimeRunning: state.Clock.isTimeRunning,
-	time: state.Clock.time
+	seconds: state.Clock.seconds
 });
 
 const mapDispatchToProps = (dispatch) => ({
