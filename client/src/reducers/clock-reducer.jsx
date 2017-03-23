079ab0a771390e3ba729872987b6c4f557713f73
Mousecop <joaskew13@gmail.com>
2017-03-23 12:56:28 -0400

    removes reducer and action of RESET_CLOCK

--- client/src/reducers/clock-reducer.jsx
+++ client/src/reducers/clock-reducer.jsx
@@ -2,16 +2,12 @@ import * as actions from '../actions/action';
 
 const initalState = {
     isTimeRunning: false,
-    seconds: 10
 };
 
 const clockReducer = (state=initalState, action) => {
     switch(action.type) {
         case actions.TOGGLE_TIME:
-            return {...state, isTimeRunning: !state.isTimeRunning};
-        case actions.RESET_CLOCK:
-            console.log('reducer hit');
-            return ({seconds: 20, isTimeRunning: false});
+            return ({isTimeRunning: !state.isTimeRunning});
         default:
             return state;
     }
