8ad3efbb0fe4c3868aee75338f39e1662acf295b
Mousecop <joaskew13@gmail.com>
2017-03-23 19:52:13 -0400

    error

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@
 const githubIds = {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL:'https://pomogitto.herokuapp.com//api/auth/github/callback',
+    callbackURL:'https://pomo-gitto.herokuapp.com/api/auth/github/callback',
     databaseURL: process.env.DATABASE_URL
 }
 
