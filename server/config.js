690282abd088a6add50689fabbffb814894dcc94
Aaron Gottlieb <aticus19@gmail.com>
2017-03-24 08:38:52 -0600

    adds pommohistory checkmarks

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@
 const githubIds = {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL:'https://pomogitto.herokuapp.com//api/auth/github/callback',
+    callbackURL:'http://localhost:8080/api/auth/github/callback',
     databaseURL: process.env.DATABASE_URL
 }
 
