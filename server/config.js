f9c04409201959da1b706e88ed9c6aae74fc0e64
Mousecop <joaskew13@gmail.com>
2017-06-23 16:57:12 -0400

    config file

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 
 const githubIds = {
-    clientID: process.env.GITHUB_CLIENT_ID,
+    clientID: 'c7b2c8e37d76f08dcb09',
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL:'https://pomo-gitto.herokuapp.com/api/auth/github/callback',
     databaseURL: process.env.DATABASE_URL
