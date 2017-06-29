961273293b97fbcabc2a7b864f8743ae074f5631
Mousecop <joaskew13@gmail.com>
2017-06-29 12:56:39 -0400

    adds action to the reducer

--- client/src/reducers/clock-reducer.jsx
+++ client/src/reducers/clock-reducer.jsx
@@ -3,12 +3,15 @@ import * as actions from '../actions/action';
 
 const initalState = {
     isTimeRunning: false,
+    timeEnded: false
 };
 
 const clockReducer = (state=initalState, action) => {
     switch(action.type) {
         case actions.TOGGLE_TIME:
             return ({isTimeRunning: !state.isTimeRunning});
+        case actions.TIME_ENDED:
+            return{...state, timeEnded: true}
         default:
             return state;
     }
